1953021175afa4e266bfbd224e1cbeb1940c3001
tominovak33 <tominovak33@gmail.com>
2015-06-23 14:27:09 +0100

    Added basic funtions to get repo name and url

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function get_api_response($url) {
+    $api_url = DOMAIN . $url;
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $api_url);
+    //curl_setopt($curl, CURLOPT_USERPWD, USER.':'.PASSWORD);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    $returned_data = curl_exec($curl);
+    curl_close($curl);
+
+    return json_decode($returned_data);
+}
+
+function get_repo_name($repo) {
+    return $repo->full_name;
+}
+
+function get_repo_html_url($repo) {
+    return $repo->html_url;
+}

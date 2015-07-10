5f0380d64e1404b06706419233994c2cfe8d7006
Tomi Novak <tominovak33@gmail.com>
2015-07-11 00:24:36 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require ('dev_cache.php');
+require ('config/constants.php');
+require ('features.php');
+
+//$api_response = get_all_org_repositories('org-name-here');
+$api_response = get_all_repositories('tominovak33');
+
+$all_repositories = $api_response['body'];
+
+$clone_urls = get_repository_clone_urls($all_repositories);
+write_commands_to_file($clone_urls);
+
+echo "<pre>";
+var_dump($clone_urls);

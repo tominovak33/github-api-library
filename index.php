4f0ea3392bc99d1af7ba09ac33acab5e7d5ce494
Tomi Novak <tominovak33@gmail.com>
2015-07-11 00:25:23 +0100

    add comments

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $api_response = get_all_repositories('tominovak33');
 $all_repositories = $api_response['body'];
 
 $clone_urls = get_repository_clone_urls($all_repositories);
-write_commands_to_file($clone_urls);
+write_commands_to_file($clone_urls); //then run this file from the command line (maybe set up a cron? )
 
 echo "<pre>";
 var_dump($clone_urls);
